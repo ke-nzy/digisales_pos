3b18d2dee376123f9faabd4180e8f20eda7db462
Tevin Obai <tevinobai0409@gmail.com>
2024-06-19 16:41:09 +0300

    added Loader to report pages

--- src/app/(root)/inventory/page.tsx
+++ src/app/(root)/inventory/page.tsx
@@ -9,6 +9,52 @@ import { useAuthStore } from "~/store/auth-store";
 const InventoryPage = () => {
   const { site_company } = useAuthStore();
   const { inventory, loading, error } = useBranchInventory();
+  if (loading)
+    return (
+      <main className="flex min-h-[60vh] flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div
+          role="status"
+          className="max-w-md animate-pulse space-y-4 divide-y divide-gray-200 rounded border border-gray-200 p-4 shadow dark:divide-gray-700 dark:border-gray-700 md:p-6"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </main>
+    );
   return (
     <DashboardLayout title={site_company?.branch ?? ""}>
       <main className="flex min-h-[60vh] flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
