232ed3cef352e1b458caf0065992773134689b75
Tevin Obai <tevinobai0409@gmail.com>
2024-09-05 17:08:50 +0300

    cashier collections beta

--- src/lib/menu-list.ts
+++ src/lib/menu-list.ts
@@ -32,125 +32,7 @@ export function getMenuList(
   isBranchManager: boolean | undefined,
   isAdmin: boolean | undefined,
 ): Group[] {
-  if (isBranchManager) {
-    return [
-      {
-        groupLabel: "",
-        menus: [
-          {
-            href: "/dashboard",
-            label: "Dashboard",
-            active: pathname.includes("/dashboard"),
-            icon: LayoutGrid,
-            submenus: [],
-          },
-        ],
-      },
-      {
-        groupLabel: "Items & Inventory",
-        menus: [
-          {
-            href: "/inventory",
-            label: "Inventory",
-            active: pathname.includes("/products"),
-            icon: SquarePen,
-            submenus: [
-              {
-                href: "/inventory",
-                label: "Branch Inventory",
-                active: pathname === "/inventory",
-              },
-              // {
-              //   href: "/inventory/stocktake",
-              //   label: "Stock Take",
-              //   active: pathname === "/inventory/stocktake",
-              // },
-            ],
-          },
-          // {
-          //   href: "/categories",
-          //   label: "Categories",
-          //   active: pathname.includes("/categories"),
-          //   icon: Bookmark,
-          //   submenus: [],
-          // },
-        ],
-      },
-      {
-        groupLabel: "Sales",
-        menus: [
-          {
-            href: "/reports",
-            label: "Sales Reports",
-            active: pathname.includes("/products"),
-            icon: ScrollText,
-            submenus: [
-              {
-                href: "/reports/itemized",
-                label: "Itemized Sales Report",
-                active: pathname === "/itemized",
-              },
-              {
-                href: "/reports/zReport",
-                label: "Z Report",
-                active: pathname === "/zReport",
-              },
-              {
-                href: "/reports/general",
-                label: "Daily Sales Report",
-                active: pathname === "/general",
-              },
-              // {
-              //   href: "/posts/new",
-              //   label: "New Post",
-              //   active: pathname === "/posts/new",
-              // },
-            ],
-          },
-          {
-            href: "/transactions",
-            label: "Transactions",
-            active: pathname.includes("/categories"),
-            icon: ArrowLeftRight,
-            submenus: [],
-          },
-          // {
-          //   href: "/clearance",
-          //   label: "Cashier Clearance",
-          //   active: pathname.includes("/clearance"),
-          //   icon: UserCheck,
-          //   submenus: [],
-          // },
-          // {
-          //   href: "/reservations",
-          //   label: "Reservations",
-          //   active: pathname.includes("/reservations"),
-          //   icon: ShoppingBag,
-          //   submenus: [],
-          // },
-        ],
-      },
-      {
-        groupLabel: "Settings",
-        menus: [
-          // {
-          //   href: "/users",
-          //   label: "Users",
-          //   active: pathname.includes("/users"),
-          //   icon: Users,
-          //   submenus: [],
-          // },
-          {
-            href: "/account",
-            label: "Account",
-            active: pathname.includes("/account"),
-            icon: Settings,
-            submenus: [],
-          },
-        ],
-      },
-    ];
-  } else if (isAdmin) {
+  if (isAdmin) {
     return [
       {
         groupLabel: "",
@@ -239,6 +121,124 @@ export function getMenuList(
             icon: UserCheck,
             submenus: [],
           },
+          // {
+          //   href: "/reservations",
+          //   label: "Reservations",
+          //   active: pathname.includes("/reservations"),
+          //   icon: ShoppingBag,
+          //   submenus: [],
+          // },
+        ],
+      },
+      {
+        groupLabel: "Settings",
+        menus: [
+          // {
+          //   href: "/users",
+          //   label: "Users",
+          //   active: pathname.includes("/users"),
+          //   icon: Users,
+          //   submenus: [],
+          // },
+          {
+            href: "/account",
+            label: "Account",
+            active: pathname.includes("/account"),
+            icon: Settings,
+            submenus: [],
+          },
+        ],
+      },
+    ];
+  } else if (isBranchManager) {
+    return [
+      {
+        groupLabel: "",
+        menus: [
+          {
+            href: "/dashboard",
+            label: "Dashboard",
+            active: pathname.includes("/dashboard"),
+            icon: LayoutGrid,
+            submenus: [],
+          },
+        ],
+      },
+      {
+        groupLabel: "Items & Inventory",
+        menus: [
+          {
+            href: "/inventory",
+            label: "Inventory",
+            active: pathname.includes("/products"),
+            icon: SquarePen,
+            submenus: [
+              {
+                href: "/inventory",
+                label: "Branch Inventory",
+                active: pathname === "/inventory",
+              },
+              // {
+              //   href: "/inventory/stocktake",
+              //   label: "Stock Take",
+              //   active: pathname === "/inventory/stocktake",
+              // },
+            ],
+          },
+          // {
+          //   href: "/categories",
+          //   label: "Categories",
+          //   active: pathname.includes("/categories"),
+          //   icon: Bookmark,
+          //   submenus: [],
+          // },
+        ],
+      },
+      {
+        groupLabel: "Sales",
+        menus: [
+          {
+            href: "/reports",
+            label: "Sales Reports",
+            active: pathname.includes("/products"),
+            icon: ScrollText,
+            submenus: [
+              {
+                href: "/reports/itemized",
+                label: "Itemized Sales Report",
+                active: pathname === "/itemized",
+              },
+              {
+                href: "/reports/zReport",
+                label: "Z Report",
+                active: pathname === "/zReport",
+              },
+              {
+                href: "/reports/general",
+                label: "Daily Sales Report",
+                active: pathname === "/general",
+              },
+              // {
+              //   href: "/posts/new",
+              //   label: "New Post",
+              //   active: pathname === "/posts/new",
+              // },
+            ],
+          },
+          {
+            href: "/transactions",
+            label: "Transactions",
+            active: pathname.includes("/categories"),
+            icon: ArrowLeftRight,
+            submenus: [],
+          },
+          // {
+          //   href: "/clearance",
+          //   label: "Cashier Clearance",
+          //   active: pathname.includes("/clearance"),
+          //   icon: UserCheck,
+          //   submenus: [],
+          // },
           // {
           //   href: "/reservations",
           //   label: "Reservations",
